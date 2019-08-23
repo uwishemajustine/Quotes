dbee0e81bc94d79107d744a2d3ed8ec1039da8a1
uwishemajustine <alsenjsatine90@gmail.com>
2019-08-23 17:46:03 +0200

    correcting codes to be display

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,13 @@
+// export class Quote {
+//   id: number;
+//   qoute: string;
+//   author: string;
+//   publisher:string;
+// }
 export class Quote {
-  id: number;
-  qoute: string;
-  author: string;
-  publisher:string;
-}
-    
+  showDescription: boolean;
+  constructor(public id: number,public quote: string,public author: string, public publisher: string){
+    this.showDescription=true;
+  }
+}   
      
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -2,29 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-goal',
-  templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
 
-    quote:Quote = [
-    {id:1, quote:' A man who dares to waste one hour of time has not discovered the value of life', author:'Charles Darwin', publisher:'William Shakespeare'},
-    {id:2, quote: 'My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style', author:'Maya Angelou ', publisher:'C. J. Cherryh'},
-    {id:3, quote:'The most beautiful things in the world cannot be seen or even touched. They must be felt with the heart.', author:'Helen Keller', publisher:'Robert Benchley'},
-    {id:4, quote: 'Those who truly want something will keep trying until they die', author:'Maxime Lagacé', publisher:'Andrew Rohrer'},
-    {id:5, quote: 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.', author:'Norman Vincent Peale', publisher:'Ernest Hemingway'},
-    {id:6, quote: 'You just can’t beat the person who never gives up.', author:'Babe Ruth', publisher:'Jessica Valenti'},
-    {id:7, quote: 'Work while they sleep. Learn while they party. Save while they spend. Live like they dream', author:'Syed Balkhi', publisher:'Neil Gaiman'},
+    quote:Quote [] = [
+   new Quote (1, 'A man who dares to waste one hour of time has not discovered the value of life', 'Charles Darwin', 'William Shakespeare'),
+   new Quote (2, 'My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style', 'Maya Angelou ', 'C. J. Cherryh'),
+   new Quote (3, 'The most beautiful things in the world cannot be seen or even touched. They must be felt with the heart.', 'Helen Keller', 'Robert Benchley'),
+   new Quote (4, 'Those who truly want something will keep trying until they die', 'Maxime Lagacé', 'Andrew Rohrer'),
+   new Quote (5, 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.', 'Norman Vincent Peale', 'Ernest Hemingway'),
+   new Quote (6, 'You just can’t beat the person who never gives up.', 'Babe Ruth', 'Jessica Valenti'),
+   new Quote (7, 'Work while they sleep. Learn while they party. Save while they spend. Live like they dream', 'Syed Balkhi', 'Neil Gaiman'),
     ];
-    // quoteDetails(index){
-    //   this.quote[index].showDescription = !this.quote[index].showDescription;
-    // }
-    // completeQuote(isComplete, index){
-    //   if (isComplete) {
-    //     this.quote.splice(index,0);
-    //   }
-    // }
+    quoteDetails(index){
+      this.quote[index].showDescription = !this.quote[index].showDescription;
+    }
+    completeQuote(isComplete, index){
+      if (isComplete) {
+        this.quote.splice(index,1);
+      }
+    }
     constructor() {}
 
   ngOnInit() {
